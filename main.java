91cd237960ba7004720f00e4d7f8e10f48cfcdd7
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-10-31 08:17:48 -0300

    .

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Esse e o Screen Match");
+    System.out.println("Filme: A Era do Gelo");
+    
+    int ano = 2002;
+    boolean incluidoNoPlano = true;
+    double NotaFilme = 4.5;
+    
+    System.out.println("Ano de lançamento " + ano);
+  }
+}
+  
